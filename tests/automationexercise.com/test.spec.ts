a382f804e83715bf1a3d582677f7910bcf4dfc78
rmouelle <rmouelle@users.noreply.github.com>
2026-05-28 10:05:05 +0100

    add new projects + timeouts + workflow dispatch

--- tests/automationexercise.com/test.spec.ts
+++ tests/automationexercise.com/test.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test("can open the website", async ({ page }) => {
-    await page.goto('https://www.automationexercise.com');
+    await page.goto('');
 
     await expect(page).toHaveTitle(/Automation Exercise/);
 
     const title = await page
         .locator('.features_items .title')
         .first()
-        .textContent();
+        .textContent({ timeout: 3000 });
 
     await expect(title).toContain('Features Items');
 });
