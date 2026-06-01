eaea88c68082750b4a5d1edbb6c868f597fd11be
rmouelle <rmouelle@users.noreply.github.com>
2026-06-01 11:19:56 +0100

    fix the URL in global setup

--- tests/automationexercise.com/global-setup.ts
+++ tests/automationexercise.com/global-setup.ts
@@ -4,7 +4,7 @@ export default async function globalSetup() {
     const browser = await chromium.launch();
     const page = await browser.newPage();
 
-    await page.goto('/login');
+    await page.goto(`${process.env.BASE_URL}/login`);
     await page.locator('input[data-qa="login-email"]').fill(process.env.USER_EMAIL || '');
     await page.locator('input[data-qa="login-password"]').fill(process.env.USER_PASSWORD || '');
     await page.locator('button[data-qa="login-button"]').click();
